b93e2060a4e5ce4ab8b7518cc382acebab2e7d2f
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2017-06-12 22:19:44 +0200

    extra pensja

---
+++ losowanie_ekstra_pensji/js/scripts.js
@@ -0,0 +1,45 @@
+(function () {
+    var btn = document.querySelector('.getNumbers');
+    var firstOutput = document.querySelector('.showNumbers');
+    var secOutput = document.querySelector('.showExtraNumber');
+    
+    function getRandom(min, max) {
+        
+        return Math.round(Math.random() *(max - min) + min);
+        
+    }
+    function firstNumbers() {
+        
+        var numbers = [];
+        var random;
+        for (var i = 0; i < 5; i++) {
+            random = getRandom(1, 49);
+            
+            while(numbers.indexOf(random) !== -1) {
+                console.log('Powtorzylas sie liczba ' + random);
+                random = getRandom(1, 49);
+            }
+            numbers.push(random);
+        }
+        numbers.sort(function(a, b){
+            return a - b;
+        });
+        return numbers;
+        
+    }
+    
+    function secondNumber() {
+        var number = getRandom(1, 4);
+        return number;
+    }
+    
+    function showRandomNumbers() {
+        
+//        console.log(numbers);
+        firstOutput.value = firstNumbers().join(', ');
+        secOutput.value = secondNumber();
+    }
+    
+    btn.addEventListener('click', showRandomNumbers, false);
+    
+})();
