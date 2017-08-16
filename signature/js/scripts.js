6884af631081f032470bfa984a479f21209d3cde
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2017-08-16 23:46:54 +0200

    add electronic signature

--- signature/js/scripts.js
+++ signature/js/scripts.js
@@ -2,16 +2,40 @@
     
     var signature = {
         
+        enableDrawing: function() {
+          
+            this.mouseDown = true;
+            
+        },
+        
+        disableDrawing: function() {
+            
+            this.mouseDown = false;
+            
+        },
+        
         drawLines: function(e) {
           
-            
+            if(!this.mouseDown) {
+                return;
+            }
+            console.log(e);
             
         },
         
         setupCanvas: function() {
             
+            this.mouseDown = false;
             this.ctx = this.canvas.getContext('2d');
+            
+            this.ctx.lineWidth = 5;
+            this.ctx.lineJoin = "round";
+            this.ctx.lineCap = "round";
+            this.ctx.strokeStyle = '#000000';
+            
             this.canvas.onmousemove = this.drawLines.bind(this);
+            this.canvas.onmousedown = this.enableDrawing.bind(this);
+            this.canvas.onmouseup = this.disableDrawing.bind(this);
             
         },
         
