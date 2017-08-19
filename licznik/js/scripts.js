22d1dc110cb3fc614054d13b86dd09c23d2fd36c
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2017-08-19 23:04:57 +0200

    add new counter

--- licznik/js/scripts.js
+++ licznik/js/scripts.js
@@ -1,19 +1,70 @@
-var count = document.querySelector('h2');
+var count = document.querySelectorAll('h3');
+var noRepeat = true;
+//var counter = 0;
 
-var counter = 0;
+//function meter(number, time, mark) {
+//    
+//    function c() {
+//        if(counter <= number) {
+//            count.textContent = counter++ + mark;
+//            setTimeout(c, time);
+//        }
+//    }
+//    c();
+//}
 
-function meter(number, time, mark) {
-    
-    function c() {
+//function goInterval(el, counter) {
+//    
+//    var goCounter = setInterval(function(){
+//        
+//        
+//        if (counter <= 100) {
+//            el.textContent = counter++ + '%';
+//        } else {
+//            
+//            clearInterval(goCounter);
+//        }
+//    }, 100);
+//
+//}
 
-        if(counter <= number) {
-            count.textContent = counter++ + mark;
-            setTimeout(c, time);
-        }
+function startCounter() {
+
+
+count.forEach(function(el) {
+//    noRepeat = 0;
+    var counter = 0;
+//    console.log(Math.round(scrollY));
+//    console.log((el.offsetTop));
+    if((el.offsetTop - el.offsetTop) < Math.round(scrollY) && noRepeat) {
+//        console.log('tak');
+//        console.log(el);
+//        console.log((el.offsetTop - 100));
+
+//            goInterval(el, counter);
+        
+        var goCounter = setInterval(function(){
+        
+            noRepeat = false;
+        
+            if (counter <= 100) {
+                el.textContent = counter++ + '%';
+            } else {
+
+                clearInterval(goCounter);
+            }
+        }, 100);
         
     }
-    c();
+    
+    console.log(noRepeat);
+});
+
+
 }
 
-meter(100, 10, ' %');
+document.addEventListener('scroll', function() {
 
+    startCounter();
+    
+}, false);
