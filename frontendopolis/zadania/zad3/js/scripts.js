3d02fd8f9e7c0fe330128ee5f5e13472d369bd92
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2017-11-10 23:58:47 +0100

    forntendopolis zad3

--- frontendopolis/zadania/zad3/js/scripts.js
+++ frontendopolis/zadania/zad3/js/scripts.js
@@ -1,28 +1,12 @@
-var a = document.querySelector('#a');
-var b = document.querySelector('#b');
-var output = document.querySelector('output');
-var btn = document.querySelector('.btn');
-var btnRandom = document.querySelector('.btn--random');
+var counter = document.querySelector('.counter');
+var button = document.querySelector('.btn');
+var counterValue = 1;
+counter.textContent = counterValue;
 
+button.addEventListener('click', increaseCounter(), false );
 
-btn.addEventListener('click', function(){
-    countNumber(a.value, b.value);
-}, false);
-
-btnRandom.addEventListener('click', function() {
-    countNumber(randomNumbers(), randomNumbers());
-}, false);
-
-function countNumber(a, b) {
+function increaseCounter() {
     
-    var addition = parseInt(a) + parseInt(b);
-    var subtraction = parseInt(a) - parseInt(b);
-    var multiplication = parseInt(a) * parseInt(b);
-    var division = parseInt(a) / parseInt(b);
     
-    output.textContent = (a + ' + ' +  b + ' = ' + addition) + '\n' + (a + ' - '+  b + ' = ' + subtraction) + '\n' + (a + ' * ' +  b + ' = '+ multiplication) + '\n' + (a + ' / '+  b + ' = ' + division);
-}
-
-function randomNumbers() {
-    return Math.floor((Math.random() * 20) + 1);
+    
 }
