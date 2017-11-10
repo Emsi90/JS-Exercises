914638c89e12fc31f5523c349c21668fa3804bf4
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2017-11-11 00:34:47 +0100

    forntendopolis zad3

--- frontendopolis/zadania/zad3/js/scripts.js
+++ frontendopolis/zadania/zad3/js/scripts.js
@@ -1,12 +1,21 @@
 var counter = document.querySelector('.counter');
 var button = document.querySelector('.btn');
-var counterValue = 1;
+var counterValue = 0;
 counter.textContent = counterValue;
+var click = 0;
+var increase = 0;
 
-button.addEventListener('click', increaseCounter(), false );
+button.addEventListener('click', increaseCounter, false );
 
 function increaseCounter() {
     
     
+    if(click === 5) {
+        increase += 1;
+        click = 0;
+    }
     
+    click += 1;
+    counterValue += 1 + increase;
+    counter.textContent = counterValue;
 }
